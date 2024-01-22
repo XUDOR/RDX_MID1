ee880d723783ee700f312f3439f3e16e59d4b8a7
Roderick Shoolbraid <rdxenv@gmail.com>
2024-01-21 16:50:37 -0800

    intial

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const PORT = 3000; // Use your port number here
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
 app.set('view engine', 'ejs');
